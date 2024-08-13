da87ae2f6bc853cafe4c6d143d0e0592ce84711c
Titonatos <titanium4ik777@gmail.com>
2024-08-13 20:16:23 +0300

    minor updates

---
+++ frontend/src/utils/socketHadlers.js
@@ -0,0 +1,57 @@
+import { toast } from 'react-toastify';
+import { messagesApi } from './api/messagesApi.js';
+import { channelsApi } from './api/channelsApi.js';
+
+const handleNewMessage = (newMessage) => {
+    store.dispatch(
+      messagesApi.util.updateQueryData(
+        'getMessages',
+        undefined,
+        (draftMessages) => { draftMessages.push(newMessage); },
+      ),
+    );
+  };
+
+  const handleNewChannel = (newChannel) => {
+    store.dispatch(
+      channelsApi.util.updateQueryData(
+        'getChannels',
+        undefined,
+        (draftChannels) => { draftChannels.push(newChannel); },
+      ),
+    );
+    toast.success(i18n.t('chat.notify.addChannel'));
+  };
+
+  const handleRenameChannel = ({ id, name }) => {
+    store.dispatch(
+      channelsApi.util.updateQueryData(
+        'getChannels',
+        undefined,
+        (draftChannels) => {
+          const channelIndexToUpdate = draftChannels.findIndex((channel) => channel.id === id);
+          const link = draftChannels;
+          link[channelIndexToUpdate].name = name;
+        },
+      ),
+    );
+    toast.success(i18n.t('chat.notify.renameChannel'));
+  };
+
+  const handleDeleteChannel = ({ id }) => {
+    store.dispatch(
+      channelsApi.util.updateQueryData(
+        'getChannels',
+        undefined,
+        (draft) => draft.filter((channel) => channel.id !== id),
+      ),
+    );
+    toast.success(i18n.t('chat.notify.removeChannel'));
+  };
+
+export default {
+    handleNewMessage,
+    handleNewChannel,
+    handleRenameChannel,
+    handleDeleteChannel
+};
