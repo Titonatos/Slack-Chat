c09952bffd35a681df02c604e7a653c28cbb240b
Titonatos <titanium4ik777@gmail.com>
2024-09-21 00:52:45 +0300

    upd: socket url hardcode fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import init from './Init';
 
 const chatApp = async () => {
-  const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
-  const socket = io(URL, {
-    autoConnect: false,
-  });
+  const socket = io('');
   const root = ReactDOM.createRoot(document.getElementById('chat'));
   root.render(await init(socket));
 };
